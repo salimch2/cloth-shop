6466a95fb5323707dd2341ffc7204062cadeb633
Chemlal Salim <salimch2.sc@gmail.com>
2020-07-20 12:10:24 +0100

    first comit

--- src/component/sign-in/sign-in.component.jsx
+++ src/component/sign-in/sign-in.component.jsx
@@ -4,7 +4,7 @@ import "./sign-in.style.scss";
 
 import FormInput from "../form-input.component/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
-import { signInWithGoogle } from "../../firebase/firebase.utile";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utile";
 
 class SignIn extends React.Component {
   constructor(props) {
@@ -15,9 +15,15 @@ class SignIn extends React.Component {
     };
   }
 
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
-    this.setState({ email: "", password: "" });
+    const { email, password } = this.state;
+    try {
+      await auth().signInWithEmailAndPassword(email, password);
+      this.setState({ email: "", password: "" });
+    } catch (error) {
+      console.error(error);
+    }
   };
   handleChange = (event) => {
     const { value, name } = event.target;
@@ -48,11 +54,10 @@ class SignIn extends React.Component {
             required
           />
           <div className="buttons">
-
-          <CustomButton type="submit">Sign In</CustomButton>
-          <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
-            Sign In with Google
-          </CustomButton>
+            <CustomButton type="submit">Sign In</CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+              Sign In with Google
+            </CustomButton>
           </div>
         </form>
       </div>
--- src/pages/signin-and-signup/signin-and-signup.component.jsx
+++ src/pages/signin-and-signup/signin-and-signup.component.jsx
@@ -3,11 +3,12 @@ import React from "react";
 import "./signin-and-signup.style.scss";
 
 import  SignIn  from "../../component/sign-in/sign-in.component";
+import  SignUp  from "../../component/sign-up/sign-up.component";
 
 const SignInAndSignUp = () => (
   <div className="SignInAndSignUp">
-    {" "}
     <SignIn />
+    <SignUp />
   </div>
 );
 export default SignInAndSignUp;
