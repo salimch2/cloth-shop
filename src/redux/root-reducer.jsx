3452139f64cebcf654c74c64afb0b9643580b124
Chemlal Salim <salimch2.sc@gmail.com>
2020-07-22 19:24:53 +0100

    added storage of cart item in the redux presist

--- src/redux/root-reducer.jsx
+++ src/redux/root-reducer.jsx
@@ -1,9 +1,18 @@
 import { combineReducers } from "redux";
-
 import userReducer from "./user/user.reducer";
 import cardReducer from "./cart/card.reducer";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-export default combineReducers({
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["card"],
+};
+
+const rootReducer =  combineReducers({
   user: userReducer,
   card: cardReducer,
 });
+
+export default persistReducer(persistConfig , rootReducer)
