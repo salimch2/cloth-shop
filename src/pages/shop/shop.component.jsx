3eaec22d148118fc42f7153c45797bec3637c6cb
Chemlal Salim <salimch2.sc@gmail.com>
2020-07-24 20:50:48 +0100

    add the collection or category pages

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import {Route} from 'react-router-dom'
 import CollectionsOverview from '../../component/collection-overview/collection-overview.component'
-const ShopPage = ({ collections }) => (
+import collectionPage from '../../pages/collection/collection.component'
+const ShopPage = ({match}) => {
+  console.log(match);
+  return(
   <div className="shop-page">
-    <CollectionsOverview />
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route  path={`${match.path}/:collectionId`} component={collectionPage} />
   </div>
-);
+);}
 
 
 
--- src/redux/shop/shop.selectors.jsx
+++ src/redux/shop/shop.selectors.jsx
@@ -1,10 +1,23 @@
-import {createSelector} from 'reselect'
+import { createSelector } from "reselect";
 
-const selectShop = state => state.shop;
+const selectShop = (state) => state.shop;
+
+const COLLECTION_ID_MAP = {
+  hats: 1,
+  sneakers: 2,
+  jackets: 3,
+  womens: 4,
+  men: 5,
+};
 
 export const selectCollections = createSelector(
-    [selectShop],
-    
-    shop=>shop.collections
+  [selectShop],
 
-)
+  (shop) => shop.collections
+);
+export const selectCollection = (collectionUrlParm) =>
+  createSelector([selectCollections], (collections) =>
+    collections.find(
+      (collection) => collection.id === COLLECTION_ID_MAP[collectionUrlParm]
+    )
+  );
